067809c1904409e1161c934486027b866b7d88f5
Max Poliakov <48586126+Jaimies@users.noreply.github.com>
2021-12-05 12:48:03 +0200

    test(Board): add more test cases

--- BoardTest.cpp
+++ BoardTest.cpp
@@ -25,50 +25,6 @@ TEST(BoardTest, MoveCountIsCorrectInStartPosition) {
     ASSERT_EQ(197281, countMoves(board->copy(), 4));
 }
 
-TEST(BoardTest, MoveCountIsCorrectInPosition5) {
-    Board *board = Board::fromFenString("r3k2r/pb3p2/5npp/n2p4/1p1PPB2/6P1/P2N1PBP/R3K2R b KQkq -", Piece::Black);
-    ASSERT_EQ(29, countMoves(board->copy(), 1));
-    ASSERT_EQ(953, countMoves(board->copy(), 2));
-    ASSERT_EQ(27990, countMoves(board->copy(), 3));
-    ASSERT_EQ(909807, countMoves(board->copy(), 4));
-    ASSERT_EQ(26957954, countMoves(board->copy(), 5));
-}
-
-TEST(BoardTest, MoveCountIsCorrectInBehtingPosition) {
-    Board *board = Board::fromFenString("8/8/7p/3KNN1k/2p4p/8/3P2p1/8 w - - ; bm Kc6");
-    ASSERT_EQ(25, countMoves(board->copy(), 1));
-    ASSERT_EQ(180, countMoves(board->copy(), 2));
-    ASSERT_EQ(4098, countMoves(board->copy(), 3));
-    ASSERT_EQ(46270, countMoves(board->copy(), 4));
-}
-
-TEST(BoardTest, MoveCountIsCorrectInPositionWithPins) {
-    Board *board = Board::fromFenString("8/Rbk5/8/8/8/8/8/2K5", Piece::Black);
-    ASSERT_EQ(7, countMoves(board->copy(), 1));
-    ASSERT_EQ(91, countMoves(board->copy(), 2));
-    ASSERT_EQ(1060, countMoves(board->copy(), 3));
-    ASSERT_EQ(18010, countMoves(board->copy(), 4));
-}
-
-TEST(BoardTest, MoveCountIsCorrectInPositionWherePawnCanCaptureAndPromote) {
-    Board *board = Board::fromFenString("b7/1P6/8/3K3k/8/8/8/8 w - -");
-    ASSERT_EQ(12, countMoves(board->copy(), 1));
-    ASSERT_EQ(68, countMoves(board->copy(), 2));
-    ASSERT_EQ(957, countMoves(board->copy(), 3));
-    ASSERT_EQ(7428, countMoves(board->copy(), 4));
-    ASSERT_EQ(116285, countMoves(board->copy(), 5));
-}
-
-TEST(BoardTest, DoesntAllowEnPassantWhereDoingSoExposesTheKing) {
-    Board *board = Board::fromFenString("8/3p1p2/8/r1P1K1Pq/8/8/8/7k", Piece::Black);
-    board->makeMove(new NormalMove(51, 35));
-
-    ASSERT_EQ(7, countMoves(board->copy(), 1));
-    ASSERT_EQ(184, countMoves(board->copy(), 2));
-    ASSERT_EQ(997, countMoves(board->copy(), 3));
-    ASSERT_EQ(30436, countMoves(board->copy(), 4));
-}
-
 TEST(BoardTest, MoveCountIsCorrectInPosition2) {
     Board *board = Board::fromFenString("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
 
@@ -88,4 +44,70 @@ TEST(BoardTest, MoveCountIsCorrectInPosition3) {
     ASSERT_EQ(43238, countMoves(board->copy(), 4));
     ASSERT_EQ(674624, countMoves(board->copy(), 5));
     ASSERT_EQ(11030083, countMoves(board->copy(), 6));
+}
+
+TEST(BoardTest, MoveCountIsCorrectInPosition4) {
+    Board *board = Board::fromFenString("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
+
+    ASSERT_EQ(6, countMoves(board->copy(), 1));
+    ASSERT_EQ(264, countMoves(board->copy(), 2));
+    ASSERT_EQ(9467, countMoves(board->copy(), 3));
+    ASSERT_EQ(422333, countMoves(board->copy(), 4));
+    ASSERT_EQ(15833292, countMoves(board->copy(), 5));
+    ASSERT_EQ(706045033, countMoves(board->copy(), 6));
+}
+
+TEST(BoardTest, MoveCountIsCorrectInPosition5) {
+    Board *board = Board::fromFenString("r3k2r/pb3p2/5npp/n2p4/1p1PPB2/6P1/P2N1PBP/R3K2R b KQkq -", Piece::Black);
+    ASSERT_EQ(29, countMoves(board->copy(), 1));
+    ASSERT_EQ(953, countMoves(board->copy(), 2));
+    ASSERT_EQ(27990, countMoves(board->copy(), 3));
+    ASSERT_EQ(909807, countMoves(board->copy(), 4));
+    ASSERT_EQ(26957954, countMoves(board->copy(), 5));
+}
+
+TEST(BoardTest, MoveCountIsCorrectInPosition6) {
+    Board *board = Board::fromFenString("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
+
+    ASSERT_EQ(46, countMoves(board->copy(), 1));
+    ASSERT_EQ(2079, countMoves(board->copy(), 2));
+    ASSERT_EQ(89890, countMoves(board->copy(), 3));
+    ASSERT_EQ(3894594, countMoves(board->copy(), 4));
+    ASSERT_EQ(164075551, countMoves(board->copy(), 5));
+}
+
+
+TEST(BoardTest, MoveCountIsCorrectInPositionWherePawnCanCaptureAndPromote) {
+    Board *board = Board::fromFenString("b7/1P6/8/3K3k/8/8/8/8 w - -");
+    ASSERT_EQ(12, countMoves(board->copy(), 1));
+    ASSERT_EQ(68, countMoves(board->copy(), 2));
+    ASSERT_EQ(957, countMoves(board->copy(), 3));
+    ASSERT_EQ(7428, countMoves(board->copy(), 4));
+    ASSERT_EQ(116285, countMoves(board->copy(), 5));
+}
+
+TEST(BoardTest, MoveCountIsCorrectInBehtingPosition) {
+    Board *board = Board::fromFenString("8/8/7p/3KNN1k/2p4p/8/3P2p1/8 w - - ; bm Kc6");
+    ASSERT_EQ(25, countMoves(board->copy(), 1));
+    ASSERT_EQ(180, countMoves(board->copy(), 2));
+    ASSERT_EQ(4098, countMoves(board->copy(), 3));
+    ASSERT_EQ(46270, countMoves(board->copy(), 4));
+}
+
+TEST(BoardTest, DoesntAllowEnPassantWhereDoingSoExposesTheKing) {
+    Board *board = Board::fromFenString("8/3p1p2/8/r1P1K1Pq/8/8/8/7k", Piece::Black);
+    board->makeMove(new NormalMove(51, 35));
+
+    ASSERT_EQ(7, countMoves(board->copy(), 1));
+    ASSERT_EQ(184, countMoves(board->copy(), 2));
+    ASSERT_EQ(997, countMoves(board->copy(), 3));
+    ASSERT_EQ(30436, countMoves(board->copy(), 4));
+}
+
+TEST(BoardTest, MoveCountIsCorrectInPositionWithPins) {
+    Board *board = Board::fromFenString("8/Rbk5/8/8/8/8/8/2K5", Piece::Black);
+    ASSERT_EQ(7, countMoves(board->copy(), 1));
+    ASSERT_EQ(91, countMoves(board->copy(), 2));
+    ASSERT_EQ(1060, countMoves(board->copy(), 3));
+    ASSERT_EQ(18010, countMoves(board->copy(), 4));
 }
